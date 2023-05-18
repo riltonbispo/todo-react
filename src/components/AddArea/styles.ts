d70792f0a1a5ef816903488ac7074789f207ccbb
riltonbispo <riltonbispo1@gmail.com>
2023-05-18 17:32:37 -0300

    styled: add item

---
+++ src/components/AddArea/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border: 1px solid #555;
+  border-radius: 15px;
+  padding: 10px;
+  margin-block: 20px;
+  display: flex;
+  align-items: center;
+
+  .image {
+    margin-right: 10px;
+  }
+
+  input {
+    border: 0;
+    background: transparent;
+    outline: 0;
+    color: white;
+    font-size: 14px;
+    flex: 1;
+  }
+`
