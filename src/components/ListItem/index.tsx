f988f2ed5f97eb278b33fe2a545f0562b348147a
riltonbispo <riltonbispo1@gmail.com>
2023-05-18 16:57:28 -0300

    feat: create component list item

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,7 +1,22 @@
-import * as C from './styles'
+import * as C from "./styles";
+import { Item } from "../../types/Item";
+import { useState } from "react";
 
-export const ListItem = () => {
+type Props = {
+  item: Item;
+};
+
+export const ListItem = ({ item }: Props) => {
+  const [isChecked, setIsChecked] = useState(item.done);
+
   return (
-    <C.Container>list item</C.Container>
-  )
-}
+    <C.Container done={isChecked}>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={e => setIsChecked(e.target.checked)}
+      />
+      <label> {item.name}</label>
+    </C.Container>
+  );
+};
