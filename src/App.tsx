7f841e82d74a1c5515013ce7b96a01b5ce68d312
riltonbispo <riltonbispo1@gmail.com>
2023-05-18 16:57:52 -0300

    feate: usando componente list item

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import * as C from './App.styles'
+import { useState } from "react";
+
+import * as C from "./App.styles";
+import { Item } from "./types/Item";
+import { ListItem } from "./components/ListItem";
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,
+      name: "dormid 8hrs",
+      done: true,
+    },
+    {
+      id: 2,
+      name: "ir ao mercado",
+      done: false,
+    },
+  ]);
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Header</C.Header>
+        {list.map((item, index)=>(
+          <ListItem item={item} key={index}/>
+        ))}
       </C.Area>
     </C.Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
