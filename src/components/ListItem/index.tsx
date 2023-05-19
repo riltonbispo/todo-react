a7c27e96e81a3e7a62a61e5f800b1f2cf7340001
riltonbispo <riltonbispo1@gmail.com>
2023-05-19 16:53:15 -0300

    feat: funcionalidade de remover task

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,31 +1,39 @@
 import * as C from "./styles";
 import { Item } from "../../types/Item";
-import { useState } from "react";
 
 type Props = {
   item: Item;
   itemKey: number
   handleTask: (item: number) => void
+  removeTask: (item: number) => void
 };
 
-export const ListItem = ({ item, itemKey, handleTask }: Props) => {
-  const [isChecked, setIsChecked] = useState(item.done);
-  
-  {console.log(itemKey)}
+export const ListItem = ({ item, itemKey, handleTask, removeTask }: Props) => {
 
   const handleItemDone = () => {
     handleTask(itemKey)
   }
 
+  const removeOneTask = () => {
+    removeTask(itemKey)
+  }
+
   return (
     <C.Container done={item.done}>
-      <input
-        type="checkbox"
-        checked={item.done}
-        onChange={handleItemDone}
-      />
-      <label> {item.name} - </label>
-      <span>{item.done === true ? 'Feito' : 'A Fazer'}</span>
+      <div className="item">
+        <input
+          type="checkbox"
+          checked={item.done}
+          onChange={handleItemDone}
+        />
+
+        <div className="item">
+          <label> {item.name} - </label>
+          <span>{item.done === true ? 'Feito' : 'A Fazer'}</span>
+        </div>
+      </div>
+
+      <button onClick={removeOneTask}>remover</button>
     </C.Container>
   );
 };
