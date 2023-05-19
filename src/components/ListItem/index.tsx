4340e46bf7d9913f93c2d25527545c5e4b7c81bc
riltonbispo <riltonbispo1@gmail.com>
2023-05-19 16:13:29 -0300

    feat: add funcionalidade de check task

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -4,19 +4,28 @@ import { useState } from "react";
 
 type Props = {
   item: Item;
+  itemKey: number
+  handleTask: (item: number) => void
 };
 
-export const ListItem = ({ item }: Props) => {
+export const ListItem = ({ item, itemKey, handleTask }: Props) => {
   const [isChecked, setIsChecked] = useState(item.done);
+  
+  {console.log(itemKey)}
+
+  const handleItemDone = () => {
+    handleTask(itemKey)
+  }
 
   return (
-    <C.Container done={isChecked}>
+    <C.Container done={item.done}>
       <input
         type="checkbox"
-        checked={isChecked}
-        onChange={e => setIsChecked(e.target.checked)}
+        checked={item.done}
+        onChange={handleItemDone}
       />
-      <label> {item.name}</label>
+      <label> {item.name} - </label>
+      <span>{item.done === true ? 'Feito' : 'A Fazer'}</span>
     </C.Container>
   );
 };
